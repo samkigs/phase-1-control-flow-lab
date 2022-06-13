45fe16fbc7d7fb1acb6c28f84680176748cf8486
samkigs <samuel.kihuga@student.moringaschool.com>
2022-06-13 16:46:46 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if(400 >= value){
+    return "This one is on me!";
+  } else if (2001 >= value){
+      return "I will gladly take your thirty bucks.";
+  } else if (2501 >= value){
+      return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(isCity){
+  const city = 'NYC';
+  return (isCity === city ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    case 'thanks for everything' :
+      return 'Bye.'
+    default:
+  }
+
 }
